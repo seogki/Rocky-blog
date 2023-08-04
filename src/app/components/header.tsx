5c229c94f2842afd1ad474ab0dd61284e7eed542
홍석기 <skhong@eco.kr>
2023-08-04 18:06:20 +0900

    nextjs 13 emotion 적용 안되는 문제로 tailwind css 설치

--- src/app/components/header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <>
-      <header>헤더 영역</header>
-    </>
-  );
-};
---
+++ src/app/ui/header.tsx
@@ -0,0 +1,11 @@
+// "next": "13.3.4",
+
+const MyHeader = () => {
+  return (
+    <>
+      <header>헤더 영역</header>
+    </>
+  );
+};
+
+export default MyHeader;
