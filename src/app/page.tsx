70017bd2900c3d01b3cc95f356599c3b81fcb26e
홍석기 <skhong@eco.kr>
2023-08-11 15:31:28 +0900

    readme 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,22 @@ export default async function Home() {
   const list = await getPosts();
   return (
     <>
-      <div className="w-full px-4 gap-3 grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
+      <ul className="w-full px-4 gap-4 grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
         {list.map(({ title, description }, i) => (
-          <div
+          <li
             key={i}
-            className="shadow border-2 dark:border-gray-600 rounded h-56"
+            className="shadow border-2 dark:border-gray-600 rounded h-56 relative flex flex-col"
           >
             <div className="bg-gray-500 h-32 flex justify-center items-center">
               {title}
             </div>
-            <div className="text-lg pl-2">{title}</div>
-            <div className="text-base pl-2">{description}</div>
-          </div>
+            <h3 className="text-lg pt-2 px-3 truncate">{title}</h3>
+            <p className="text-base py-4 px-3 line-clamp-2 leading-8 flex-auto">
+              {description}
+            </p>
+          </li>
         ))}
-      </div>
+      </ul>
     </>
   );
 }
