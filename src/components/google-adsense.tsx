c2a41625a43e03382faa55d84f3a122545688d33
홍석기 <skhong@eco.kr>
2023-09-05 16:03:08 +0900

    fix: google adsense

--- src/components/google-adsense.tsx
+++ src/components/google-adsense.tsx
@@ -5,7 +5,7 @@ export default function GoogleAdsense() {
     <>
       {process.env.NEXT_PUBLIC_APP_GA_PUB_KEY && (
         <Script
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_APP_GA_PUB_KEY}"
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_APP_GA_PUB_KEY}
           `}
           crossOrigin="anonymous"
         ></Script>
