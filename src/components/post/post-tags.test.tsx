179c7fdfade68627838eaa7f9392145a509f42f4
홍석기 <skhong@eco.kr>
2023-09-06 13:22:48 +0900

    bug: slug bug로 인해 임시 롤백

--- src/components/post/post-tags.test.tsx
+++ src/components/post/post-tags.test.tsx
@@ -2,9 +2,11 @@ import { render, screen } from "@testing-library/react";
 import PostTags from "./post-tags";
 
 test("should return three tags", async () => {
-  render(<PostTags tags={["test1", "test2", "test3"]} />);
+  const { findAllByText } = render(
+    <PostTags tags={["test1", "test2", "test3"]} />
+  );
 
-  const tags = await screen.findAllByRole("textbox");
+  const tags = await findAllByText(/test/i);
 
   expect(tags).toHaveLength(3);
 });
