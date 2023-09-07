2fd1dff5fbe3fa35e1b098c1de59bacaafdee097
홍석기 <skhong@eco.kr>
2023-09-07 11:16:27 +0900

    fix: line 색상 수정

--- src/components/post/post-item-aside.tsx
+++ src/components/post/post-item-aside.tsx
@@ -10,7 +10,23 @@ type Props = {
 
 export default function PostItemAside({ toc }: Props) {
   const pathname = usePathname();
+  return (
+    <>
+      <aside className="basis-3/12 hidden lg:block ml-4 border-l-2 border-zinc-100 dark:border-zinc-800">
+        <div className="fixed mt-16 mr-4 drop-shadow-md">
+          <section className="text-zinc-700 dark:text-zinc-300 max-w-[250px] max-h-[350px] overscroll-contain rounded-b-lg py-2 overflow-auto px-4">
+            <h2 className="font-bold text-base border-b-2 border-zinc-200 dark:border-zinc-700 pb-1 w-fit">
+              📄 Table of Contents
+            </h2>
+            <TocList toc={toc} pathname={pathname} />
+          </section>
+        </div>
+      </aside>
+    </>
+  );
+}
 
+const TocList = ({ toc, pathname }: { toc: Toc[]; pathname: string }) => {
   const setIndent = (parent: string) => {
     if (parent === "h2" || parent === "h1")
       return "ml-2 font-normal text-sm xl:text-base";
@@ -23,38 +39,25 @@ export default function PostItemAside({ toc }: Props) {
   const addPrefix = (parent: string, text: string) => {
     if (parent === "h4" || parent === "h6") return `- ${text}`;
     if (parent === "h3" || parent === "h5") return `· ${text}`;
-    // if (parent !== "h1" && parent !== "h2") return `- ${text}`;
-
     return text;
   };
 
   return (
-    <>
-      <aside className="basis-3/12 hidden lg:block ml-4 border-l-2 border-zinc-500">
-        <div className="fixed mt-16 mr-4 drop-shadow-md">
-          <section className="text-zinc-700 dark:text-zinc-300 max-w-[250px] max-h-[350px] overscroll-contain rounded-b-lg py-2 overflow-auto px-4">
-            <h2 className="font-bold text-base border-b-2 border-black dark:border-white pb-1 w-fit">
-              📄 Table of Contents
-            </h2>
-            <ul>
-              {toc.map((item, idx) => (
-                <li key={idx}>
-                  <Link href={`${pathname}/${item.href}`} shallow>
-                    <p
-                      role="paragraph"
-                      className={`${setIndent(
-                        item.parent
-                      )} py-1 my-2 hover:text-teal-600 hover:dark:text-teal-400`}
-                    >
-                      {addPrefix(item.parent, item.text)}
-                    </p>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </div>
-      </aside>
-    </>
+    <ul>
+      {toc.map((item, idx) => (
+        <li key={idx}>
+          <Link href={`${pathname}/${item.href}`} shallow>
+            <p
+              role="paragraph"
+              className={`${setIndent(
+                item.parent
+              )} py-1 my-2 hover:text-teal-600 hover:dark:text-teal-400`}
+            >
+              {addPrefix(item.parent, item.text)}
+            </p>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
-}
+};
