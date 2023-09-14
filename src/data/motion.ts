299e082984fbdcf946616d44b8f8ff68a80649d7
Seogki <seogkihong333@gmail.com>
2023-09-14 23:28:26 +0900

    fix: performance 문제로 framer 수정

--- src/data/motion.ts
+++ src/data/motion.ts
@@ -2,18 +2,18 @@ export const FadeTweenMotion = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
-  transition: { type: "tween" }
+  transition: { type: "tween" },
 };
 
 export const ScaleTweenMotion = {
   initial: { scale: 0 },
   animate: { scale: 1 },
   exit: { scale: 0 },
-  transition: { type: "tween" }
+  transition: { type: "tween" },
 };
 
 export const EnterMotion = {
   initial: { opacity: 0, translateX: "20px" },
   animate: { opacity: 1, translateX: "0%" },
-  exit: { opacity: 0, translateX: "20px" }
+  exit: { opacity: 0, translateX: "20px" },
 };
