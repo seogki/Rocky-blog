7728cc5cb776a7d74dfaf5de3e873e501482d5e0
홍석기 <skhong@eco.kr>
2023-09-22 10:57:14 +0900

    seo: canonical 제거

--- src/app/posts/search/page.tsx
+++ src/app/posts/search/page.tsx
@@ -22,9 +22,9 @@ export const generateMetadata = ({ searchParams }: Props): Metadata => {
   return {
     title: `Rocky Blog - Posts Search List [${tag}]`,
     description: `This is my Rocky Blog Search List of ${tag}`,
-    alternates: {
-      canonical: `${baseUrl}/posts`
-    },
+    // alternates: {
+    //   canonical: `${baseUrl}/posts`
+    // },
     openGraph: {
       title: `Rocky Blog - Posts Search List [${tag}]`,
       description: `This is my Rocky Blog Search List of ${tag}`,
